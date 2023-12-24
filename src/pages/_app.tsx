5b1851c4e06ed68eafc85349358518790542613c
Andy Wright <atwright147@gmail.com>
2023-12-24 09:56:27 +0000

    install MUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,22 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <CssBaseline />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
