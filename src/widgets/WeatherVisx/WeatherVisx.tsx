a89db76d26d360bccd6a21a8f001e1a6142ea2dd
Andy Wright <atwright147@gmail.com>
2024-01-30 22:07:57 +0000

    fix VisX implementation

--- src/widgets/WeatherVisx/WeatherVisx.tsx
+++ src/widgets/WeatherVisx/WeatherVisx.tsx
@@ -1,27 +1,20 @@
 import { useWeather } from '@/hooks/weather/useWeather';
 import { Paper, Typography } from '@mui/material';
 import { useMeasure } from '@uidotdev/usehooks';
+import { ParentSize } from '@visx/responsive';
 import {
-  AnimatedAxis, // any of these can be non-animated equivalents
-  AnimatedGrid,
-  AnimatedLineSeries,
+  Axis, // any of these can be non-animated equivalents
+  Grid,
+  LineSeries,
   Tooltip,
   XYChart,
 } from '@visx/xychart';
 import { useEffect, useRef } from 'react';
+
 import styles from './WeatherVisx.module.scss';
 
 export const WeatherVisx = () => {
-  const [widgetRef, { height: widgetHeight }] = useMeasure();
-  const [headingRef, { height: headingHeight }] = useMeasure();
-  const heightRef = useRef(0);
   const { data, isLoading, isError } = useWeather();
-
-  useEffect(() => {
-    if (widgetHeight && headingHeight) {
-      heightRef.current = Math.abs(widgetHeight - headingHeight);
-    }
-  }, [widgetHeight, headingHeight]);
 
   // biome-ignore lint/suspicious/noExplicitAny: not sure what these types should be
   const formatData = (dataX: any, dataY: any) => {
@@ -29,7 +22,7 @@ export const WeatherVisx = () => {
     return dataX?.map((data: any, index: number) => {
       const time = new Date(dataX[index]);
       return {
-        x: `${String(time.getHours()).padStart(2, '0')}:00`,
+        x: `${String(time.getHours()).padStart(2, '0')}`,
         y: dataY[index],
       };
     });
@@ -42,46 +35,56 @@ export const WeatherVisx = () => {
     yAccessor: (d?: any) => (d ? d.y : null),
   };
 
+  const MARGIN_BIG = 45;
+  const MARGIN_SMALL = 5;
+
   return (
     <Paper
       component="section"
       className={styles.widget}
-      ref={widgetRef}
       style={{ width: 'inherit', height: 'inherit' }}
       sx={{ p: 1, boxSizing: 'border-box' }}
     >
-      <Typography variant="h6" component="h1" ref={headingRef}>
+      <Typography variant="h6" component="h1">
         Weather Forecast (Visx)
       </Typography>
 
       {!isLoading && !isError && (
-        <XYChart
-          xScale={{ type: 'band' }}
-          yScale={{ type: 'linear' }}
-          margin={{ top: 40, right: 0, bottom: 60, left: 0 }}
-          height={heightRef.current}
-        >
-          <AnimatedAxis orientation="bottom" label="time" />
-          <AnimatedGrid columns={false} numTicks={4} />
-          <AnimatedLineSeries
-            dataKey="Line 1"
-            data={formatData(data?.data?.hourly?.time, data?.data?.hourly?.temperature2m)}
-            {...accessors}
-          />
-          <Tooltip
-            snapTooltipToDatumX
-            snapTooltipToDatumY
-            showVerticalCrosshair
-            showSeriesGlyphs
-            renderTooltip={({ tooltipData }) => (
-              <div>
-                {accessors.xAccessor(tooltipData?.nearestDatum?.datum)}
-                {', '}
-                {accessors.yAccessor(tooltipData?.nearestDatum?.datum)}
-              </div>
+        <div style={{ overflow: 'hidden' }}>
+          <ParentSize debounceTime={0}>
+            {(parent) => (
+              <XYChart
+                width={parent.width}
+                height={parent.height}
+                xScale={{ type: 'band' }}
+                yScale={{ type: 'linear' }}
+                margin={{ top: 20, right: MARGIN_SMALL, bottom: MARGIN_BIG, left: MARGIN_BIG }}
+              >
+                <Axis orientation="bottom" label="Time" tickFormat={(d) => `${d}h`} />
+                <Axis orientation="left" label="Temperature" tickFormat={(d) => `${d}°C`} />
+                <Grid columns={false} numTicks={10} />
+                <LineSeries
+                  dataKey="Line 1"
+                  data={formatData(data?.data?.hourly?.time, data?.data?.hourly?.temperature2m)}
+                  {...accessors}
+                />
+                <Tooltip
+                  snapTooltipToDatumX
+                  snapTooltipToDatumY
+                  showVerticalCrosshair
+                  showSeriesGlyphs
+                  renderTooltip={({ tooltipData }) => (
+                    <div className="tooltip">
+                      {accessors.xAccessor(tooltipData?.nearestDatum?.datum)}:00h
+                      <br />
+                      {accessors.yAccessor(tooltipData?.nearestDatum?.datum).toFixed(2)}°C
+                    </div>
+                  )}
+                />
+              </XYChart>
             )}
-          />
-        </XYChart>
+          </ParentSize>
+        </div>
       )}
     </Paper>
   );
